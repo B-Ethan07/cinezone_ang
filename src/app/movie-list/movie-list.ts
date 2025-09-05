7a0832c977ab490b039d8d824da3668255db24e0
Ethan <e.boulard07@gmail.com>
2025-09-05 18:40:26 +0200

    login + filter

--- src/app/movie-list/movie-list.ts
+++ src/app/movie-list/movie-list.ts
@@ -15,6 +15,14 @@ export class MovieList {
   movies: Movie[] = [];
   loading: boolean = true;
   deletedMovie: number[] = [];
+  categories: { [id: number]: string } = {
+  1: 'Science-Fiction',
+  2: 'Drame',
+  3: 'Thriller / Social',
+  4: 'Comédie / Romance',
+  5: 'Animation / Fantastique',
+  6: 'Guerre / Historique'
+};
 
   constructor(
     private movieService: MovieService,
@@ -60,4 +68,19 @@ export class MovieList {
   onCreate() {
     this.router.navigate(['movies/create']);
   }
+  sortByCategory() {
+  this.movies.sort((a, b) => a.category_id - b.category_id);
+  }
+  getCategoryName(id: number): string {
+  return this.categories[id] || 'Inconnue';
+  }
+
+sortByYearAsc() {
+  this.movies.sort((a, b) => a.release_year - b.release_year);
+}
+
+sortByYearDesc() {
+  this.movies.sort((a, b) => b.release_year - a.release_year);
+}
+
 }
