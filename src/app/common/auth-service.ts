cf8d09907bcc26c7aab7ae83aac5b16eefce79c1
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2025-09-12 15:01:16 +0200

    feature : adding environment dev & prod

--- src/app/common/auth-service.ts
+++ src/app/common/auth-service.ts
@@ -3,40 +3,42 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  user: User | null = null;
 
-  user:User | null = null;
-
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient, private router: Router) {}
 
-signup(email: string, password: string, name: string): Observable<User> {
-  return this.http.post<User>(
-    `${this.baseUrl}/users`,
-    {
-      name: name,
-      email: email,
-      password: password,
-    },
-    {
-      withCredentials: true,
-    }
-  ).pipe(
-    tap({
-      next: (u) => {
-        this.user = u;
-      },
-      error: () => {
-        this.user = null;
-      },
-    })
-  );
-}
+  signup(email: string, password: string, name: string): Observable<User> {
+    return this.http
+      .post<User>(
+        `${this.baseUrl}/users`,
+        {
+          name: name,
+          email: email,
+          password: password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .pipe(
+        tap({
+          next: (u) => {
+            this.user = u;
+          },
+          error: () => {
+            this.user = null;
+          },
+        })
+      );
+  }
 
   login(email: string, password: string): Observable<User> {
     return this.http
@@ -62,32 +64,34 @@ signup(email: string, password: string, name: string): Observable<User> {
   profil(): Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/profile`, {
       withCredentials: true,
-    })
-  }
-    refreshProfil(): void {
-    this.http.get<User>(`${this.baseUrl}/profile`, {
-      withCredentials: true,
-    }).subscribe({
-      next: (u) => {
-        this.user = u;
-      },
-      error: ()=> {
-        this.user = null
-      }
     });
   }
+  refreshProfil(): void {
+    this.http
+      .get<User>(`${this.baseUrl}/profile`, {
+        withCredentials: true,
+      })
+      .subscribe({
+        next: (u) => {
+          this.user = u;
+        },
+        error: () => {
+          this.user = null;
+        },
+      });
+  }
 
-logout(): Observable<void> {
-  return this.http.get<void>(`${this.baseUrl}/logout`, { withCredentials: true }).pipe(
-    tap({
-      next: () => {
-        console.log('Déconnexion réussie');
-        this.user = null;
-      },
-      error: (err) => {
-        console.error('Erreur de déconnexion', err); // Ajoute ça
-      }
-    })
-  );
-}
+  logout(): Observable<void> {
+    return this.http.get<void>(`${this.baseUrl}/logout`, { withCredentials: true }).pipe(
+      tap({
+        next: () => {
+          console.log('Déconnexion réussie');
+          this.user = null;
+        },
+        error: (err) => {
+          console.error('Erreur de déconnexion', err); // Ajoute ça
+        },
+      })
+    );
+  }
 }
--- src/app/common/movie-service.ts
+++ src/app/common/movie-service.ts
@@ -2,29 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  // Je récupère l'url de mon environment.ts
+  private baseUrl = environment.apiUrl;
 
-  private baseUrl = 'http://localhost:3000/'
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient){}
-
-  getMovies(): Observable<Movie[]>{
-    return this.http.get<Movie[]>(`${this.baseUrl}movies`)
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.baseUrl}/movies`);
   }
-  getMovie(id: number): Observable<Movie>{
-    return this.http.get<Movie>(`${this.baseUrl}movies/${id}`)
+  getMovie(id: number): Observable<Movie> {
+    return this.http.get<Movie>(`${this.baseUrl}/movies/${id}`);
   }
-  createMovie(movie: Movie): Observable<Movie>{
-    return this.http.post<Movie>(`${this.baseUrl}movies`, movie, { withCredentials: true })
+  createMovie(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(`${this.baseUrl}/movies`, movie, { withCredentials: true });
   }
-  updateMovie(id: number, movie: Movie): Observable<Movie>{
-    return this.http.put<Movie>(`${this.baseUrl}movies/${id}`, movie, { withCredentials: true })
+  updateMovie(id: number, movie: Movie): Observable<Movie> {
+    return this.http.put<Movie>(`${this.baseUrl}/movies/${id}`, movie, { withCredentials: true });
   }
-  deleteMovie(id: number): Observable<void>{
-    return this.http.delete<void>(`${this.baseUrl}movies/${id}`, { withCredentials: true })
+  deleteMovie(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/movies/${id}`, { withCredentials: true });
   }
 }
